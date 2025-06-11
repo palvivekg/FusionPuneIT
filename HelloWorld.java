baafd7275b18d9e08c9dc9d1e9581e2b93beb830
palvivekg <palv75494@gmail.com>
2025-06-11 11:54:41 +0530

    RelationaloperatorDemo

---
+++ HelloWorld.java
@@ -0,0 +1,6 @@
+class HelloWorld{
+	public static void main(String[]a){
+System.out.println("hHello Vivek !!!");
+System.out.println("Welcome To Java World!!!");
+	}
+}
---
+++ RelationaloperatorDemo.java
@@ -0,0 +1,26 @@
+public class RelationaloperatorDemo{
+	public static void main(String[]a){
+		int number1 =10,number2=20;
+		
+		System.out.println(number1+"<"+number2+"is :"+(number1<number2));
+	System.out.println(number1+">"+number2+"is :"+(number1>number2));
+	System.out.println(number1+"<="+number2+"is :"+(number1<=number2));
+	System.out.println(number1+">="+number2+"is :"+(number1>=number2));
+	System.out.println(number1+"=="+number2+"is :"+(number1==number2));
+	System.out.println(number1+"!="+number2+"is :"+(number1!=number2));
+	System.out.println("---------------------------------------");
+
+
+	int number3=10;
+	int number4=10;
+	System.out.println(number1+"<"+number2+"is :"+(number3<number4));
+	System.out.println(number1+">"+number2+"is :"+(number3>number4));
+	System.out.println(number1+"<="+number2+"is :"+(number3<=number4));
+	System.out.println(number1+">="+number2+"is :"+(number3>=number4));
+	System.out.println(number1+"=="+number2+"is :"+(number3==number4));
+	System.out.println(number1+"!="+number2+"is :"+(number3!=number4));
+	
+	
+
+	}
+}
